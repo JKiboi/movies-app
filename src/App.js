84595756654a2a6bdf5beae154909a5eb3f00717
JKiboi <anythingtechke@gmail.com>
2024-12-22 15:24:37 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
   return (
     <div className="app">
       <header className="header">
-        <h1>MovieVerse</h1>
+        <h1>MovieNights</h1>
         <p>Discover your next favorite movie</p>
       </header>
 
